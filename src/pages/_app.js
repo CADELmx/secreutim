4b2c2b3ecd94c8af827e67927015e477de1652f5
Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>
2024-06-04 13:47:45 -0600

    Update _app.js (#3)
    From Layout to Layout in imports

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import { Layout } from '@/components/Layout'
+import { Layout } from '@/components/layout'
 import { ContextProvider } from '@/context'
 import '@/styles/globals.css'
 import Notify from '@/toast'
